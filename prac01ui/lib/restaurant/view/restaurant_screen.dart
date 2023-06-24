5b2c4fc19aab86255020804c14d2d08b158d8ecf
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-25 00:00:20 +0900

    feature: data model 만들기: 하지만 너무 반복적이고 비효율적인 방법

--- prac01ui/lib/restaurant/view/restaurant_screen.dart
+++ prac01ui/lib/restaurant/view/restaurant_screen.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:prac01ui/common/const/data.dart';
 import 'package:prac01ui/restaurant/component/restaurant_card.dart';
+import 'package:prac01ui/restaurant/model/restaurant_model.dart';
 
 class RestaurantScreen extends StatelessWidget {
   const RestaurantScreen({super.key});
@@ -45,19 +46,36 @@ class RestaurantScreen extends StatelessWidget {
                 itemBuilder: (_, index) {
                   // itemBuilder가 실행될 때마다 item에 하나씩 담김
                   final item = snapshot.data![index];
-                  return RestaurantCard(
-                    restaurantImage: Image.network(
-                      'http://$ip${item['thumbUrl']}',
-                      fit: BoxFit.cover,
-                    ),
+
+                  // parsed item
+                  final pItem = RestaurantModel(
+                    id: item['id'],
                     name: item['name'],
-                    // Future로 받아오게 되면 타입이 dynamic인 상황이 있는데
-                    // 우리는 해당 타입이 String인 것을 알기 때문에 타입.from으로 바꿔주면 됩니다.
+                    thumbUrl: item['thumbUrl'],
                     tags: List<String>.from(item['tags']),
+                    // values를 하고 firstWhere를 하면 enum의 모든 값을 순회하며
+                    // 동일한 값을 찾는다.
+                    priceRange: RestaurantPriceRange.values.firstWhere(
+                      (element) => element.name == item['priceRange'],
+                    ),
+                    ratings: item['ratings'],
                     ratingsCount: item['ratingsCount'],
                     deliveryTime: item['deliveryTime'],
                     deliveryFee: item['deliveryFee'],
-                    ratings: item['ratings'],
+                  );
+                  return RestaurantCard(
+                    restaurantImage: Image.network(
+                      'http://$ip${pItem.thumbUrl}',
+                      fit: BoxFit.cover,
+                    ),
+                    name: pItem.name,
+                    // Future로 받아오게 되면 타입이 dynamic인 상황이 있는데
+                    // 우리는 해당 타입이 String인 것을 알기 때문에 타입.from으로 바꿔주면 됩니다.
+                    tags: pItem.tags,
+                    ratingsCount: pItem.ratingsCount,
+                    deliveryTime: pItem.deliveryTime,
+                    deliveryFee: pItem.deliveryFee,
+                    ratings: pItem.ratings,
                   );
                 },
               );
