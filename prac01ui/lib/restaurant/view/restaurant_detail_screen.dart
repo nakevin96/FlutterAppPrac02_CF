f1de596bb96c2966d210258f1d53ca892d16adcd
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-25 10:40:44 +0900

    feature: CustomScrollView를 활용해서 스크롤 뷰 만들기

--- prac01ui/lib/restaurant/view/restaurant_detail_screen.dart
+++ prac01ui/lib/restaurant/view/restaurant_detail_screen.dart
@@ -12,26 +12,86 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '이세계 아이돌',
-      child: Column(
-        children: [
-          RestaurantCard(
-            restaurantImage: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '이세돌 최고',
-            tags: const ['아이네', '징버거', '릴파'],
-            ratingsCount: 50000,
-            deliveryTime: 0,
-            deliveryFee: 0,
-            ratings: 5.0,
-            isDetail: true,
-            detail: '주르르, 고세구, 비챤',
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16.0,
-            ),
-            child: ProductCard(),
-          ),
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+
+      // Column(
+      //   children: [
+      //     RestaurantCard(
+      //       restaurantImage: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+      //       name: '이세돌 최고',
+      //       tags: const ['아이네', '징버거', '릴파'],
+      //       ratingsCount: 50000,
+      //       deliveryTime: 0,
+      //       deliveryFee: 0,
+      //       ratings: 5.0,
+      //       isDetail: true,
+      //       detail: '주르르, 고세구, 비챤',
+      //     ),
+      //     const Padding(
+      //       padding: EdgeInsets.symmetric(
+      //         horizontal: 16.0,
+      //       ),
+      //       child: ProductCard(),
+      //     ),
+      //   ],
+      // ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        restaurantImage: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '이세돌 최고',
+        tags: const ['아이네', '징버거', '릴파'],
+        ratingsCount: 50000,
+        deliveryTime: 0,
+        deliveryFee: 0,
+        ratings: 5.0,
+        isDetail: true,
+        detail: '주르르, 고세구, 비챤',
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16.0,
+      ),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(
+                top: 16.0,
+              ),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
       ),
     );
   }
