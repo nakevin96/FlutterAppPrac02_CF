5d83621f95e51b16a4cd679135e39a5c1d3ea731
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-28 17:24:08 +0900

    Study: Consumer를 통해서 부분 빌드 하기

--- prac02riverpod/lib/screen/code_generation_screen.dart
+++ prac02riverpod/lib/screen/code_generation_screen.dart
@@ -18,7 +18,8 @@ class CodeGenerationScreen extends ConsumerWidget {
       number1: 4,
       number2: 4,
     ));
-    final state6 = ref.watch(gStateNotifierProvider);
+    // Consumer 위젯 안으로 옮김
+    // final state6 = ref.watch(gStateNotifierProvider);
 
     return DefaultLayout(
       title: 'CodeGenerationScreen',
@@ -40,7 +41,33 @@ class CodeGenerationScreen extends ConsumerWidget {
               loading: () => const CircularProgressIndicator(),
             ),
             Text(state5.toString()),
-            Text(state6.toString()),
+            // state6는 +, - 를 눌렀을 때 변화합니다.
+            // +, -, invalidate하면 state6만 변하는데
+            // 문제는 이 state와 상관 없는 나머지 위젯들도 전부 다시 build됩니다.
+            // state 1~6 중 하나만 실행되도 전체 위젯이 빌드가 되는 것입니다.
+            // 이를 방지하기 위해 별도 위젯으로 분리할 수 있지만 riverpod에서는 이를 비효율적이라 생각했고
+            // Consumer 위젯을 별도로 제공해주어 이 기능을 수행할 수 있게 해줍니다.
+            // Text(state6.toString()),
+            Consumer(
+              builder: (context, ref, child) {
+                // Consumer의 세번째 파라미터인 chlid는 아래와 같이 쓸 수 있습니다.
+                // 이 기능이 필요한 이유는 Consumer로 만들어낸 부분도 state가 변하는 부분을 제외한
+                // 나머지 부분들은 build될 필요가 없기 때문에
+                // build에 자원이 많이 소요되거나 state 업데이트 마다 build하고 싶지 않은 위젯을 여기에 넣어줍니다.
+                final state6 = ref.watch(gStateNotifierProvider);
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('State6: $state6'),
+                    const SizedBox(
+                      width: 16.0,
+                    ),
+                    if (child != null) child,
+                  ],
+                );
+              },
+              child: const Text('Hello I am Cousumer Child'),
+            ),
             IconButton(
               onPressed: () {
                 ref.read(gStateNotifierProvider.notifier).increment();
@@ -58,6 +85,7 @@ class CodeGenerationScreen extends ConsumerWidget {
               ),
             ),
             // invalidate() 유효하지 않게 만들기
+            //invalidate를 호출 하면 provider가 disposed되고 다시 udpate되어 초기상태로 돌아감
             ElevatedButton(
               onPressed: () {
                 ref.invalidate(gStateNotifierProvider);
