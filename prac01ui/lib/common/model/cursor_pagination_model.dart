d9ea2e22ab939e39c1a4a62a7f2c39d9c817ee3b
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-30 10:37:06 +0900

    Study: CursorPagination 작업하기

--- prac01ui/lib/common/model/cursor_pagination_model.dart
+++ prac01ui/lib/common/model/cursor_pagination_model.dart
@@ -2,11 +2,21 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
+abstract class CursorPaginationBase {}
+
+abstract class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({required this.message});
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
 @JsonSerializable(
   //genericArgumentFactories를 true로 설정하게 될 경우 generic을 고려한 파일 생성을 해줍니다.
   genericArgumentFactories: true,
 )
-class CursorPagination<T> {
+class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
 
@@ -34,4 +44,22 @@ class CursorPaginationMeta {
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
+}
+
+// listview에서 새로고침 기능 구현시 사용
+class CursorPaginationRefetching extends CursorPagination {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+// 리스트 제일 아래에 도착하여
+// 추가 데이터를 요청해야 하는 상황
+// CursorPaginationLoading은 meta속성이 없어서 이 클래스를 사용하고 상태를 표현
+class CursorPaginationFetchingMore extends CursorPagination {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
 }
